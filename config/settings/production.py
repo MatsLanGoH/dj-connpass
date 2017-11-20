512db3757a5148ec47fdda7e799868de71b1247e
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2017-11-20 14:43:04 +0900

    Heroku settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,6 +1,6 @@
 from .base import *
 
-SECRET_KEY = env('DJANGO_SECRET_KEY', default='!ol93fhqyec!5l9e32jc84_rj0)$!3fe22ghzz77)9i&m-wb3o')
+SECRET_KEY = env('DJANGO_SECRET_KEY')$!3fe22ghzz77)9i&m-wb3o')
 
 DEFAULT_FILE_STORAGE = 'config.s3utils.MediaS3BotoStorage'
 STATICFILES_STORAGE = 'config.s3utils.StaticS3BotoStorage'
@@ -9,8 +9,8 @@ ALLOWED_HOSTS = ['dj-connpass.herokuapp.com']
 
 
 # AWS S3 Storage settings
-AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID', default='AKIAIRAXS5Z6FXBNW2OQ')
-AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY', default='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')
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'djconnpass-static'
 
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
@@ -28,3 +28,4 @@ AWS_LOCATION = 'static'
 
 # Refer to this site regarding settings.
 # https://simpleisbetterthancomplex.com/tutorial/2017/08/01/how-to-setup-amazon-s3-in-a-django-project.html
+ALLOWED_HOSTS = ['*']
