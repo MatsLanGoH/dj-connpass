ba017758d58309ed3379f8834e3554bd302c6ac8
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2017-11-20 17:58:09 +0900

    Reorder production settings (cosmetic change)

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,11 +2,13 @@ from .base import *
 
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
+# Refer to this site regarding settings.
+# https://simpleisbetterthancomplex.com/tutorial/2017/08/01/how-to-setup-amazon-s3-in-a-django-project.html
+
 DEFAULT_FILE_STORAGE = 'config.s3utils.MediaS3Boto3Storage'
 STATICFILES_STORAGE = 'config.s3utils.StaticS3Boto3Storage'
 
 ALLOWED_HOSTS = ['dj-connpass.herokuapp.com']
-# ALLOWED_HOSTS = ['*']
 
 
 # AWS S3 Storage settings
@@ -18,6 +20,7 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 STATIC_DIRECTORY = '/static/'
 MEDIA_DIRECTORY = '/media/'
+
 STATIC_URL = AWS_S3_CUSTOM_DOMAIN + STATIC_DIRECTORY
 MEDIA_URL = AWS_S3_CUSTOM_DOMAIN + MEDIA_DIRECTORY
 
@@ -27,5 +30,3 @@ AWS_S3_OBJECT_PARAMETERS = {
 
 AWS_LOCATION = 'static'
 
-# Refer to this site regarding settings.
-# https://simpleisbetterthancomplex.com/tutorial/2017/08/01/how-to-setup-amazon-s3-in-a-django-project.html
